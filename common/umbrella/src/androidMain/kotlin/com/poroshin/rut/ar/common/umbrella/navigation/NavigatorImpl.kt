19c63ec08f517230650bc4cd5bfd9340177ccc09
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-10 19:10:37 +0300

    [Android] Implement navigator

---
+++ common/umbrella/src/androidMain/kotlin/com/poroshin/rut/ar/common/umbrella/navigation/NavigatorImpl.kt
@@ -0,0 +1,32 @@
+package com.poroshin.rut.ar.common.umbrella.navigation
+
+import android.os.Bundle
+import com.github.terrakok.cicerone.Screen
+import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+
+class NavigatorImpl : Navigator {
+
+    override fun navigateTo(router: Router, key: NavigationTree, params: Bundle) {
+        router.navigateTo(getFragmentScreen(key, params))
+    }
+
+    override fun startFlow(router: Router, key: NavigationTree, params: Bundle) {
+        (router as? FlowRouter)?.startFlow(getFragmentScreen(key, params))
+            ?: router.navigateTo(getFragmentScreen(key, params))
+    }
+
+    override fun newRootScreen(router: Router, key: NavigationTree, params: Bundle) {
+        router.newRootScreen(getFragmentScreen(key, params))
+    }
+
+    private fun getFragmentScreen(key: NavigationTree, params: Bundle): Screen {
+        return when (key) {
+            NavigationTree.Plp -> FragmentScreen { PlaceholderFragment.newInstance("PLP") }
+            NavigationTree.Pdp -> FragmentScreen { PlaceholderFragment.newInstance("PDP") }
+            NavigationTree.Ar -> FragmentScreen { PlaceholderFragment.newInstance("AR") }
+        }
+    }
+}
+
+
