c7d62cabb4ad24d8b2a6acf3adadcddb04e3d922
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-10 19:22:31 +0300

    [Android] Add navigator and router DI

--- common/umbrella/src/androidMain/kotlin/com/poroshin/rut/ar/common/umbrella/di/PlatformModule.android.kt
+++ common/umbrella/src/androidMain/kotlin/com/poroshin/rut/ar/common/umbrella/di/PlatformModule.android.kt
@@ -2,9 +2,18 @@ package com.poroshin.rut.ar.common.umbrella.di
 
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import com.github.terrakok.cicerone.Cicerone
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
+import com.poroshin.rut.ar.common.umbrella.navigation.FlowRouter
+import com.poroshin.rut.ar.common.umbrella.navigation.Navigator
+import com.poroshin.rut.ar.common.umbrella.navigation.NavigatorImpl
 
 val androidPlatformModule: Module = module {
-    // Android platform-specific bindings
+    val cicerone: Cicerone<Router> = Cicerone.create(FlowRouter(null))
+
+    single<Navigator> { NavigatorImpl() }
+    single<FlowRouter> { cicerone.router as FlowRouter }
 }
 
 
