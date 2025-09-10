2eed7c67b06419cdac5c1055fd414e4f089ee50e
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-10 12:38:06 +0300

    [iOS] Create viewmodel holder in ios. Add some instruments to work with flows and coroutines from swift

--- iosApp/iosApp/MVI/VMHolder.swift
+++ iosApp/iosApp/MVI/VMHolder.swift
@@ -6,12 +6,15 @@
 //  Copyright © 2025 orgName. All rights reserved.
 //
 
+import ARApp
 import Foundation
 
+// MARK: - Generic VMHolder
 final class VMHolder<VM: AnyObject, State: AnyObject, Action: AnyObject>: ObservableObject {
     let vm: VM
+
     @Published private(set) var state: State
-    private var bag: ?
+    private var disposableHandle: Kotlinx_coroutines_coreDisposableHandle?
 
     init(vm: VM, initialState: State) {
         self.vm = vm
@@ -19,11 +22,74 @@ final class VMHolder<VM: AnyObject, State: AnyObject, Action: AnyObject>: Observ
     }
 
     func start(
-        bind: (VM, @escaping (State) -> Void, @escaping (Action) -> Void) -> Kotlinx_coroutines_coreDisposableHandle,
-        onAction: @escaping (Action) -> Void
+        stateFlow: Kotlinx_coroutines_coreFlow,
+        actionFlow: Kotlinx_coroutines_coreFlow,
+        onAction: @escaping (Action) -> Void = { _ in }
     ) {
-        bag = bind(vm, { [weak self] s in self?.state = s }, onAction)
+        disposableHandle = FlowWatchUtilsKt.bind(
+            state: stateFlow,
+            onState: { [weak self] newState in
+                if let typedState = newState as? State {
+                    DispatchQueue.main.async {
+                        self?.state = typedState
+                    }
+                }
+            },
+            action: actionFlow,
+            onAction: { action in
+                if let typedAction = action as? Action {
+                    onAction(typedAction)
+                }
+            }
+        )
     }
 
-    func stop() { bag?.dispose(); bag = nil }
+    func stop() {
+        disposableHandle?.dispose()
+        disposableHandle = nil
+    }
+}
+
+// MARK: - SharedViewModel VMHolder
+final class SharedVMHolder<VM: SharedViewModel, State: AnyObject, Action: AnyObject, Event: AnyObject>: ObservableObject {
+    let viewModel: VM
+    @Published private(set) var state: State
+    private var disposableHandle: Kotlinx_coroutines_coreDisposableHandle?
+    
+    init(viewModel: VM, initialState: State) {
+        self.viewModel = viewModel
+        self.state = initialState
+    }
+    
+    func start(onAction: @escaping (Action) -> Void = { _ in }) {
+        disposableHandle = FlowWatchUtilsKt.bind(
+            state: viewModel.viewState,
+            onState: { [weak self] newState in
+                if let typedState = newState as? State {
+                    DispatchQueue.main.async {
+                        self?.state = typedState
+                    }
+                }
+            },
+            action: viewModel.viewAction,
+            onAction: { action in
+                if let typedAction = action as? Action {
+                    onAction(typedAction)
+                }
+            }
+        )
+    }
+    
+    func sendEvent(_ event: Event) {
+        viewModel.onEvent(event: event)
+    }
+    
+    func stop() {
+        disposableHandle?.dispose()
+        disposableHandle = nil
+    }
+    
+    deinit {
+        stop()
+    }
 }
