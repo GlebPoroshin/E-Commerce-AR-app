07d5d5482fc2b15dc8023db748c7b67ef946662a
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-11 09:33:27 +0300

    [Android] Rewrite activity to use new navigation

--- androidApp/src/main/kotlin/com/poroshin/rut/ar/android/MainActivity.kt
+++ androidApp/src/main/kotlin/com/poroshin/rut/ar/android/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.poroshin.rut.ar.android
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import android.view.View
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentContainerView
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.poroshin.rut.ar.common.umbrella.navigation.NavigationTree
 import com.poroshin.rut.ar.common.umbrella.navigation.Navigator
 import com.poroshin.rut.ar.common.umbrella.navigation.FlowRouter
-import com.poroshin.rut.ar.common.umbrella.navigation.PlaceholderFragment
 import org.koin.android.ext.android.inject
 
 class MainActivity : FragmentActivity() {
@@ -16,19 +22,30 @@ class MainActivity : FragmentActivity() {
     private val navigator: Navigator by inject()
     private val router: FlowRouter by inject()
 
-    private val appNavigator by lazy { AppNavigator(this, R.id.container) }
+    private val containerId: Int = View.generateViewId()
+    private val appNavigator by lazy { AppNavigator(this, containerId) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            navigator.newRootScreen(router, NavigationTree.Plp, Bundle())
+        setContent {
+            AndroidView(
+                modifier = Modifier.fillMaxSize(),
+                factory = { context ->
+                    FragmentContainerView(context).apply {
+                        id = containerId
+                    }
+                },
+            )
+            LaunchedEffect(Unit) {
+                if (savedInstanceState == null) {
+                    navigator.newRootScreen(router, NavigationTree.Plp)
+                }
+            }
         }
     }
 
-    override fun onResumeFragments() {
-        super.onResumeFragments()
+    override fun onResume() {
+        super.onResume()
         navigatorHolder.setNavigator(appNavigator)
     }
 
