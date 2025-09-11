7d439eb734ea837c4caa26f819a28c6d4cd145e7
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-11 09:32:47 +0300

    [Android] Add Kdoc to NavigationTree and refactor DI

--- common/umbrella/src/androidMain/kotlin/com/poroshin/rut/ar/common/umbrella/di/PlatformModule.android.kt
+++ common/umbrella/src/androidMain/kotlin/com/poroshin/rut/ar/common/umbrella/di/PlatformModule.android.kt
@@ -10,10 +10,10 @@ import com.poroshin.rut.ar.common.umbrella.navigation.Navigator
 import com.poroshin.rut.ar.common.umbrella.navigation.NavigatorImpl
 
 val androidPlatformModule: Module = module {
-    val cicerone: Cicerone<Router> = Cicerone.create(FlowRouter(null))
-
+    single<Cicerone<FlowRouter>> { Cicerone.create(FlowRouter(null)) }
+    single<FlowRouter> { get<Cicerone<FlowRouter>>().router }
+    single<NavigatorHolder> { get<Cicerone<FlowRouter>>().getNavigatorHolder() }
     single<Navigator> { NavigatorImpl() }
-    single<FlowRouter> { cicerone.router as FlowRouter }
 }
 
 
