c447083b6b866ae1a576b1c7dfb268fe43053b33
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-13 23:16:59 +0300

    [KMP] Add pdp event for load model

--- common/pdp/presentation/src/commonMain/kotlin/com/poroshin/rut/ar/common/pdp/presentation/model/PdpEvent.kt
+++ common/pdp/presentation/src/commonMain/kotlin/com/poroshin/rut/ar/common/pdp/presentation/model/PdpEvent.kt
@@ -4,6 +4,13 @@ import com.poroshin.rut.ar.common.mvi.UiEvent
 
 sealed class PdpEvent : UiEvent {
     data class OnCreate(val sku: Long) : PdpEvent()
+
+    class OnModelLoad(
+        val sku: Long,
+        val url: String,
+        val version: Int,
+    ) : PdpEvent()
+
     data object OnRetry : PdpEvent()
 }
 
