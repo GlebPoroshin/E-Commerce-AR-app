70297ec1cc64f8d43b697008c3a66472aa0f41eb
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-13 19:52:54 +0300

    [KMP] Add get platform fun

---
+++ common/pdp/domain/src/androidMain/kotlin/com/poroshin/rut/ar/common/pdp/domain/Platform.android.kt
@@ -0,0 +1,5 @@
+package com.poroshin.rut.ar.common.pdp.domain
+
+actual fun currentOs(): OsType = OsType.ANDROID
+
+
---
+++ common/pdp/domain/src/commonMain/kotlin/com/poroshin/rut/ar/common/pdp/domain/Platform.kt
@@ -0,0 +1,7 @@
+package com.poroshin.rut.ar.common.pdp.domain
+
+enum class OsType { ANDROID, IOS }
+
+expect fun currentOs(): OsType
+
+
