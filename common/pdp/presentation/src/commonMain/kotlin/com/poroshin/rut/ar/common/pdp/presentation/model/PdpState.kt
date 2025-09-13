a5066d3e9d07f2778028afc57f643d255beac334
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-13 23:17:14 +0300

    [KMP] Add model loading progress to state

--- common/pdp/presentation/src/commonMain/kotlin/com/poroshin/rut/ar/common/pdp/presentation/model/PdpState.kt
+++ common/pdp/presentation/src/commonMain/kotlin/com/poroshin/rut/ar/common/pdp/presentation/model/PdpState.kt
@@ -5,8 +5,11 @@ import com.poroshin.rut.ar.common.pdp.domain.ProductPageInfo
 
 sealed class PdpState : UiState {
     data object Loading : PdpState()
-    class Content(val product: ProductPageInfo) : PdpState()
+
+    data class Content(
+        val loadingState: Int? = null,
+        val product: ProductPageInfo,
+    ) : PdpState()
+
     data class Error(val message: String) : PdpState()
 }
-
-
