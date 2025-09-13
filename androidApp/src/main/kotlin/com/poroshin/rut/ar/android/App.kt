817f7b6a68757f8a8b58bb8271c6bd2326174167
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-13 21:19:29 +0300

    [KMP][Android][iOS] Add some deps and small refactoring

--- androidApp/src/main/kotlin/com/poroshin/rut/ar/android/App.kt
+++ androidApp/src/main/kotlin/com/poroshin/rut/ar/android/App.kt
@@ -1,15 +1,20 @@
 package com.poroshin.rut.ar.android
 
 import android.app.Application
+import android.content.Context
 import com.poroshin.rut.ar.common.umbrella.di.androidPlatformModule
 import com.poroshin.rut.ar.common.umbrella.di.umbrellaCommonModules
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin { modules(umbrellaCommonModules() + androidPlatformModule) }
+        startKoin {
+            modules(umbrellaCommonModules()
+                    + androidPlatformModule
+                    + module { single<Context> { this@App } }
+            )
+        }
     }
 }
-
-
