e3e9559f599d81f9e15573cbe127ffd1c5dd6664
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-09-14 12:04:48 +0300

    [KMP] Change usecase to return path

--- common/pdp/data/src/commonMain/kotlin/com/poroshin/rut/ar/common/pdp/data/usecase/DownloadProductModelUseCaseImpl.kt
+++ common/pdp/data/src/commonMain/kotlin/com/poroshin/rut/ar/common/pdp/data/usecase/DownloadProductModelUseCaseImpl.kt
@@ -8,32 +8,29 @@ import com.poroshin.rut.ar.common.pdp.domain.usecase.DownloadProductModelUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
+import kotlinx.io.files.Path
 
 class DownloadProductModelUseCaseImpl(
     private val modelRepository: ModelRepository,
 ) : DownloadProductModelUseCase {
+
     override suspend fun invoke(
         sku: Long,
         url: String,
         version: Int,
         onProgress: (received: Long, total: Long?) -> Unit,
-    ) = withContext(Dispatchers.IO) {
+    ): Path = withContext(Dispatchers.IO) {
         try {
+            val objectModelType = objectModelType()
+            val path = getDownloadPath("ar/models/${sku}${objectModelType}")
             val currentVersion = modelRepository.checkExistingModel(sku)
 
-            if (currentVersion == version) return@withContext
+            if (currentVersion != version) {
+                modelRepository.downLoadModel(sku, path, url, onProgress)
+                modelRepository.saveModelVersion(sku, version)
+            }
 
-            val objectModelType = objectModelType()
-            val path = getDownloadPath( "ar/models/${sku}${objectModelType}")
-
-            modelRepository.downLoadModel(
-                sku = sku,
-                url = url,
-                path = path,
-                onProgress = onProgress,
-            )
-
-            modelRepository.saveModelVersion(sku, version)
+            path
         } catch (e: Exception) {
             throw ModelLoadException(e.message ?: "Произошла ошикбка : $e")
         }
