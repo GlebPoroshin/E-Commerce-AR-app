bddee573494cf4471a06b29bf965a6129494eb52
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-11-01 22:43:12 +0300

    [KMP] Add isModelExists to pdp state

--- common/pdp/presentation/src/commonMain/kotlin/com/poroshin/rut/ar/common/pdp/presentation/model/PdpState.kt
+++ common/pdp/presentation/src/commonMain/kotlin/com/poroshin/rut/ar/common/pdp/presentation/model/PdpState.kt
@@ -9,6 +9,7 @@ sealed class PdpState : UiState {
     data class Content(
         val loadingState: Int? = null,
         val product: ProductPageInfo,
+        val isModelExists: Boolean = false,
     ) : PdpState()
 
 }
